a99a4e7a5b8146ad90d84b3632f665710f66d347
Kazurus <akazurus@gmail.com>
2022-12-23 23:53:50 +0300

    feat: add draft for pdf processor

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use questionnaire_transformer::temp;
+use questionnaire_transformer::infra::pdf_questions_processor::PdfQuestionsProcessor;
+use std::path::PathBuf;
 
 fn main() {
-    println!("Hello, world! {}", temp());
+    let path = PathBuf::from("./target/temp2.pdf");
+    // let absolute_path = fs::canonicalize(&path.as_path()).unwrap();
+
+    let processor = PdfQuestionsProcessor::from(path);
+    processor.parse();
 }
