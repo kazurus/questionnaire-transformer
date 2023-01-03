0fc6555ed68580c6f4daf6e8a9b7bb8546cb10b8
Kazurus <akazurus@gmail.com>
2023-01-03 16:06:00 +0300

    feat: adapt processor for Xodo html
    Resolve #71 #46 #62 #63 #66

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use questionnaire_transformer::infra::html_questions_processor::HtmlQuestionsPro
 use std::path::PathBuf;
 
 fn main() {
-    let path = PathBuf::from("./target/temp.html");
+    let path = PathBuf::from("./target/temp3.html");
     // let absolute_path = fs::canonicalize(&path.as_path()).unwrap();
 
     let processor = HtmlQuestionsProcessor::from(path);
