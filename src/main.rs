6812742dc46fe8bf92a03e466e98c29bef90e1b1
Kazurus <akazurus@gmail.com>
2023-01-06 21:57:15 +0300

    feat: add csv repository
    Resolve #54

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use questionnaire_transformer::infra::html_questions_processor::HtmlQuestionsProcessor;
 use std::path::PathBuf;
 
-fn main() {
-    let path = PathBuf::from("./target/temp3.html");
-    // let absolute_path = fs::canonicalize(&path.as_path()).unwrap();
+use questionnaire_transformer::infra::csv_questions_repository::CsvQuestionsRepository;
+use questionnaire_transformer::infra::html_questions_processor::HtmlQuestionsProcessor;
 
-    let processor = HtmlQuestionsProcessor::from(path);
-    processor.parse();
+fn main() {
+    let html_file_path = PathBuf::from("./target/temp3.html");
+    // let absolute_path = fs::canonicalize(&path.as_path()).unwrap();
+    let processor = HtmlQuestionsProcessor::from(html_file_path);
+
+    let questions = processor.parse();
+
+    let csv_file_path = PathBuf::from("./target/temp.csv");
+    let csv_repo = CsvQuestionsRepository::from(csv_file_path);
+    csv_repo
+        .save_all(questions)
+        .expect("Can't save questions to csv file");
 }
