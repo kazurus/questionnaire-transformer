eee71ec1c9daca3f5a14218addfb03c57a5baea5
Alex <akazurus@gmail.com>
2023-01-08 15:27:12 +0300

    feat: add error handling to csv repo (#87)
    Resolve #86

--- src/lib/infra/csv_questions_repository.rs
+++ src/lib/infra/csv_questions_repository.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+use csv::StringRecord;
 use itertools::Itertools;
 
 use crate::domain::aggregator::questions::Questions;
@@ -9,6 +10,13 @@ use crate::domain::entity::question::Question;
 pub struct CsvQuestionsRepository {
     path: PathBuf,
 }
+
+struct QuestionFields(
+    Option<String>,
+    Option<String>,
+    Option<String>,
+    Vec<Option<(String, bool)>>,
+);
 
 impl CsvQuestionsRepository {
     pub fn save_all(&self, questions: Questions) -> Result<(), &str> {
@@ -35,45 +43,87 @@ impl CsvQuestionsRepository {
         Ok(())
     }
 
-    pub fn get_all(&self) -> Result<Questions, &str> {
-        let mut rdr = csv::ReaderBuilder::new()
+    pub fn get_all(&self) -> Result<Questions, Box<dyn std::error::Error>> {
+        let questions_vec = self
+            .parse_records_to_questions_fields()
+            .into_iter()
+            .map(|QuestionFields(question, score, max_score, answers)| {
+                let answers = answers.into_iter().collect::<Option<Vec<_>>>();
+
+                Some(Question::new(question?, score?, max_score?, answers?))
+            })
+            .enumerate()
+            .map(|(line_id, mayby_question)| {
+                mayby_question.ok_or(format!("Can't csv parse on {} line", line_id + 1))
+            })
+            .collect::<Result<Vec<_>, _>>();
+
+        Ok(Questions::from(questions_vec?))
+    }
+
+    pub fn get_all_soft(&self) -> Result<Questions, Box<dyn std::error::Error>> {
+        let questions_vec = self
+            .parse_records_to_questions_fields()
+            .into_iter()
+            .map(|QuestionFields(question, score, max_score, answers)| {
+                let answers = answers
+                    .into_iter()
+                    .filter(Option::is_some)
+                    .collect::<Option<Vec<_>>>();
+
+                Some(Question::new(
+                    question.unwrap_or_default(),
+                    score.unwrap_or_default(),
+                    max_score.unwrap_or_default(),
+                    answers.unwrap_or_default(),
+                ))
+            })
+            .enumerate()
+            .map(|(line_id, mayby_question)| {
+                mayby_question.ok_or(format!("Can't csv parse on {} line", line_id + 1))
+            })
+            .collect::<Result<Vec<_>, _>>();
+
+        Ok(Questions::from(questions_vec?))
+    }
+
+    fn parse_records_to_questions_fields(&self) -> Vec<QuestionFields> {
+        csv::ReaderBuilder::new()
             .has_headers(false)
             .from_path(&self.path)
-            .expect("Can't open csv file for reading");
+            .map_err(|e| format!("Can't open csv file for reading: {e}"))
+            .map(|r| r.into_records())
+            .into_iter()
+            .flatten()
+            .map(|record| self.map_record_to_question_fields(record.unwrap_or_default()))
+            .collect::<Vec<_>>()
+    }
 
-        let questions_vec = rdr
-            .records()
-            .map(|maybe_rec| maybe_rec.expect("Can't read csv string"))
-            .map(|record| {
-                let pos = record
-                    .position()
-                    .expect("Can't get rec position from csv")
-                    .line();
-                let msg = format!("Can't csv parse on {} line", pos);
+    fn map_record_to_question_fields(&self, record: StringRecord) -> QuestionFields {
+        let question = record.get(0).map(|q| q.to_string());
+        let score = record.get(2).map(|q| q.to_string());
+        let max_score = record.get(3).map(|q| q.to_string());
+        let answers = self
+            .parse_record_answers(record.get(1).unwrap_or_default())
+            .into_iter()
+            .collect::<Vec<Option<_>>>();
 
-                let question = record.get(0).expect(&msg).to_string();
-                let score = record.get(2).expect(&msg).to_string();
-                let max_score = record.get(3).expect(&msg).to_string();
-                let answers = record
-                    .get(1)
-                    .expect(&msg)
-                    .split('\n')
-                    .map(|line| {
-                        line.split(" - ")
-                            .collect_tuple::<(&str, &str)>()
-                            .expect("Can't collect answer tuple")
-                    })
-                    .map(|(answer, status)| match status {
-                        "true" => (answer.to_string(), true),
-                        _ => (answer.to_string(), false),
-                    })
-                    .collect::<Vec<_>>();
+        QuestionFields(question, score, max_score, answers)
+    }
 
-                Question::new(question, score, max_score, answers)
+    fn parse_record_answers(&self, answers: &str) -> Vec<Option<(String, bool)>> {
+        answers
+            .split('\n')
+            .map(|line| line.split(" - ").collect_tuple::<(&str, &str)>())
+            .map(|res| {
+                let (answer, status) = res?;
+
+                match status {
+                    "true" => Some((answer.to_string(), true)),
+                    _ => Some((answer.to_string(), false)),
+                }
             })
-            .collect::<Vec<_>>();
-
-        Ok(Questions::from(questions_vec))
+            .collect::<Vec<Option<_>>>()
     }
 }
 
