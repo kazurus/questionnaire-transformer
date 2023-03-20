d46fb8c16369aa088fa8a34cee4c0b27a12df20b
Alex <akazurus@gmail.com>
2023-03-20 23:10:25 +0300

    feat: add parser for new html file format (#102)
    Resolve #100

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::path::PathBuf;
 use std::rc::Rc;
 
 use questionnaire_transformer::infra::csv_questions_repository::CsvQuestionsRepository;
-use questionnaire_transformer::infra::html_questions_parser::HtmlQuestionsParser;
+// use questionnaire_transformer::infra::html_questions_parser::HtmlQuestionsParser;
+use questionnaire_transformer::infra::html_questions_parser_v2::HtmlQuestionsParserV2;
 
 use questionnaire_transformer::app::use_case::{
     get_all_questions::handler::GetAllQuestionsUseCaseHandler,
@@ -10,9 +11,9 @@ use questionnaire_transformer::app::use_case::{
 };
 
 fn main() {
-    let html_file_path = PathBuf::from("./target/temp3.html");
+    let html_file_path = PathBuf::from("./target/temp-html-1.html");
     // let absolute_path = fs::canonicalize(&path.as_path()).unwrap();
-    let parser = Rc::new(HtmlQuestionsParser::from(html_file_path));
+    let parser = Rc::new(HtmlQuestionsParserV2::from(html_file_path));
 
     let csv_file_path = PathBuf::from("./target/temp.csv");
     let csv_repo = Rc::new(CsvQuestionsRepository::from(csv_file_path));
